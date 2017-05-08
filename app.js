18f330d04773b77784f899846748c60f3a2b69cd
anlausch <anne-lauscher@web.de>
2017-05-08 12:30:46 +0200

    Adapt deployment to port mapping

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const SwaggerExpress = require('swagger-express-mw');
-//const SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 const swaggerUi = require('swagger-ui-express');
 const yaml = require('js-yaml');
 const fs = require('fs');
-//const swaggerDocument = require('./api/swagger/swagger.yaml');
 
 const app = require('express')();
 const mongoose = require('mongoose');
@@ -21,19 +19,22 @@ db.once('open', function() {
     accesslog.info("DB successfully opened.")
 });
 
-// TODO: Is there a better solution to this?
 var config = process.argv.indexOf("test") > 0 ? require("./test/api/config.json") : require("./config/config.json");
-var uri = "mongodb://" + config.db.host + ":" + config.db.port + "/" + config.db.schema;
+var uri = "mongodb://" + config.db.host + ":" + config.db.port + "/" + (process.env.SCHEMA || config.db.schema);
 
 mongoose.connect(uri);
 
 var swaggerDocument = yaml.safeLoad(fs.readFileSync('./api/swagger/swagger.yaml', 'utf8'));
 
+// hack for making port mapping work with swagger ui
+if(process.env.PORT_MAPPED === true && process.env.NODE_ENV === "production"){
+    swaggerDocument.basePath = "/locdb";
+}else if (process.env.PORT_MAPPED === true && process.env.NODE_ENV === "development"){
+    swaggerDocument.basePath = "/locdb-dev";
+}
+
 SwaggerExpress.create({appRoot: __dirname}, function(err, swaggerExpress) {
     if (err) { throw err; }
-
-    // add swagger-ui
-    //app.use(SwaggerUi(swaggerExpress.runner.swagger));
     app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     // install middleware
@@ -41,5 +42,4 @@ SwaggerExpress.create({appRoot: __dirname}, function(err, swaggerExpress) {
 
     var port = process.env.PORT || 80;
     app.listen(port);
-
 });
