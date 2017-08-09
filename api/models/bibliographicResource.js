be4bfd00cc0a53e2c684f37da0001bd6f495cad9
anlausch <anne-lauscher@web.de>
2017-08-09 09:52:45 +0200

    Specify elastics port (2)

--- api/models/bibliographicResource.js
+++ api/models/bibliographicResource.js
@@ -76,8 +76,7 @@ const brSchema = new Schema({
 // we want to run a single elastic for the beginning. As the model name corresponds to the index name in elastic, we make
 // sure that we do not mix up everything by adding the connection name to the index name
 brSchema.plugin(mongoosastic,{
-    index: mongoose.connection.name + '_br',
-    port: 9300
+    index: mongoose.connection.name + '_br'
 });
 
 
