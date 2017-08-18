5fc40d1aa91e4f73245e3c203bd1b1d9d0e16052
anlausch <anne-lauscher@web.de>
2017-08-18 11:58:35 +0200

    First draft of fetching feeds + test

--- api/helpers/feedHelper.js
+++ api/helpers/feedHelper.js
@@ -2,17 +2,47 @@
  * Created by anlausch on 8/18/2017.
  */
 'use strict';
-
+const request = require('request');
+const Feedparser = require('feedparser');
 const errorlog = require('./../util/logger.js').errorlog;
 
 var FeedHelper = function(){
 };
 
 
-FeedHelper.prototype.query = function(query, callback){
+FeedHelper.prototype.fetchSingle = function(feedUrl, callback){
+    var self = this;
+    // Define our streams
+    var req = request(feedUrl, {timeout: 10000});
+    var feedparser = new Feedparser();
+    var posts = [];
 
+    // Define our handlers
+    req.on('error', self.handleErrors);
+
+    req.on('response', function(res) {
+        // And boom goes the dynamite
+        res.pipe(feedparser);
+    });
+
+    feedparser.on('error', self.handleErrors);
+    feedparser.on('end', function(){
+        return callback(null, posts);
+    });
+
+    feedparser.on('readable', function() {
+        var post;
+
+        while (post = this.read()) {
+            posts.push(post);
+        }
+    });
 };
 
+FeedHelper.prototype.handleErrors = function(err, callback){
+   errorlog.error(err);
+   return callback(null, err);
+};
 
 /**
  * Factory function
