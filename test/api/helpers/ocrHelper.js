9b858317ff68fbc9593af541b63687deae805645
anlausch <anne-lauscher@web.de>
2017-10-06 10:53:44 +0200

    Add new test for OCR Helper

---
+++ test/api/helpers/ocrHelper.js
@@ -0,0 +1,34 @@
+/**
+ * Created by anlausch on 10/6/2017.
+ */
+const should = require('should');
+const setup = require('./../setup.js').createSetup();
+const ocrHelper = require('./../../../api/helpers/ocrHelper.js').createOcrHelper();
+
+describe('helpers', function() {
+    describe.only('ocrHelper', function() {
+        before(function (done) {
+            setup.dropDB(function (err) {
+                done();
+            });
+        });
+
+        after(function (done) {
+            setup.dropDB(function (err) {
+                done();
+            });
+        });
+
+        describe('triggerOCRProcessing', function () {
+            it('should return something', function (done) {
+                this.timeout(5000);
+                ocrHelper.queryOcrComponent("./../loc-db/test/api/data/ocr_example_1/0001.png", function (err, result) {
+                    console.log(result);
+                    should.not.exists(err);
+                    done();
+                });
+            });
+        });
+
+    });
+});
