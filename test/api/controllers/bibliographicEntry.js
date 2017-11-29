5d6c2f07038aa8a1f498cf61ddeb1a02485ef9ac
anlausch <anne-lauscher@web.de>
2017-11-29 09:50:39 +0100

    Incorporate new zdb marc mapping

--- test/api/controllers/bibliographicEntry.js
+++ test/api/controllers/bibliographicEntry.js
@@ -390,7 +390,7 @@ describe('controllers', function() {
                             res.body[0].should.have.property("status", status.external);
                             res.body[0].should.have.property("identifiers");
                             res.body[0].identifiers.should.be.Array;
-                            res.body[0].identifiers.should.have.lengthOf(6);
+                            res.body[0].identifiers.should.have.lengthOf(7);
                             res.body[0].identifiers[5].should.have.property("scheme", "URL_SWB");
                             done();
                         });
