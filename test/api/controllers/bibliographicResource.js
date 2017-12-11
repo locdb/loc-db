4262684bbfe8d1d6c895ec48653b1700a5de21aa
anlausch <anne-lauscher@web.de>
2017-12-11 14:41:43 +0100

    Add test for 400 error in POST bibliographicResources

--- test/api/controllers/bibliographicResource.js
+++ test/api/controllers/bibliographicResource.js
@@ -9,7 +9,7 @@ var agent = request.agent(server);
 
 describe('controllers', function() {
 
-  describe('bibliographicResource', function() {
+  describe.only('bibliographicResource', function() {
       var id = "";
       var idToDelete = "";
       before(function(done) {
@@ -582,6 +582,90 @@ describe('controllers', function() {
                       res.body[1].should.deepEqual(res.body[0].embodiedAs[0].scans[0]);
                       done();
                   });
+          });
+      });
+
+      describe('POST /bibliographicResources', function() {
+          it('should not save a br in the db', function (done) {
+              var br = {
+                  "identifiers": [
+                      {
+                          "scheme": "OCLC_ID",
+                          "literalValue": "243773523"
+                      },
+                      {
+                          "scheme": "ISBN",
+                          "literalValue": "1878379259"
+                      },
+                      {
+                          "scheme": "ISBN",
+                          "literalValue": "1878379240"
+                      },
+                      {
+                          "scheme": "SWB_PPN",
+                          "literalValue": "03890019X"
+                      },
+                      {
+                          "scheme": "URL_SWB",
+                          "literalValue": "http://swb.bsz-bw.de/DB=2.1/PPNSET?PPN=03890019X"
+                      }
+                  ],
+                  "title": "Minorities at risk :",
+                  "subtitle": "a global view of ethnopolitical conflicts /",
+                  "edition": "1. publ., 2. print.",
+                  "contributors": [
+                      {
+                          "identifiers": [],
+                          "roleType": "PUBLISHER",
+                          "heldBy": {
+                              "identifiers": [],
+                              "roleType": "PUBLISHER",
+                              "givenName": "",
+                              "familyName": "",
+                              "nameString": "United States Institute of Peace Pr.,"
+                          }
+                      },
+                      {
+                          "identifiers": [],
+                          "roleType": "AUTHOR",
+                          "heldBy": {
+                              "identifiers": [],
+                              "roleType": "AUTHOR",
+                              "givenName": "",
+                              "familyName": "",
+                              "nameString": null
+                          }
+                      },
+                      {
+                          "identifiers": [],
+                          "roleType": "AUTHOR",
+                          "heldBy": {
+                              "identifiers": [],
+                              "roleType": "AUTHOR",
+                              "givenName": "",
+                              "familyName": "",
+                              "nameString": null
+                          }
+                      }
+                  ],
+                  "publicationYear": "1993",
+                  "status": "EXTERNAL",
+                  "type": "",
+                  "containerTitle": "",
+                  "number": ""
+              };
+
+              agent
+                  .post('/bibliographicResources')
+                  .send(br)
+                  .set('Accept', 'application/json')
+                  .expect('Content-Type', /json/)
+                  .expect(400)
+                  .end(function(err, res){
+                      should.not.exist(err);
+                      done();
+                  });
+
           });
       });
 
