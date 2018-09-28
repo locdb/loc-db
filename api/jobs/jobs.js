a7fb6d77f277dadb628dd1b1c1e64c655e334292
anlausch <anne-lauscher@web.de>
2018-09-28 08:56:54 +0200

    Add missing import

--- api/jobs/jobs.js
+++ api/jobs/jobs.js
@@ -7,6 +7,7 @@ const config = require('./../../config/config');
 const logger = require('./../util/logger');
 const mongoUri = "mongodb://" + config.DB.HOST + ":" + config.DB.PORT + "/" + config.DB.SCHEMA;
 const agenda = new Agenda({db: {address: mongoUri, collection: 'agenda'}});
+const util = require('util');
 
 require('./precalculateSuggestions')(agenda);
 require('./extractReferences')(agenda);
@@ -23,20 +24,20 @@ agenda.on('fail', function(err, job) {
 
     if (job.attrs.failCount >= 2) {
 
-        extraMessage = format('too many failures, giving up');
+        extraMessage = util.format('too many failures, giving up');
 
     } else if (shouldRetry(err)) {
 
         job.attrs.nextRunAt = secondsFromNowDate(2);
 
-        extraMessage = format('will retry in %s seconds at %s',
+        extraMessage = util.format('will retry in %s seconds at %s',
             2, job.attrs.nextRunAt.toISOString());
 
         job.save();
     }
 
     if (process.env.NODE_ENV !== 'test') {
-        console.error('Agenda job [%s] %s failed with [%s] %s failCount:%s',
+        logger.error('Agenda job [%s] %s failed with [%s] %s failCount:%s',
             job.attrs.name, job.attrs._id, err.message || 'Unknown error', extraMessage, job.attrs.failCount);
     }
 
@@ -46,7 +47,7 @@ function shouldRetry(err) {
 
     // Retry on connection errors as they may just be temporary
     if (/(ECONNRESET|ECONNREFUSED)/.test(err.message)) {
-        return true;
+        return false;
     }
     return false;
 }
