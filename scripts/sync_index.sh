19c5da98a1a5af9bd32b483a17b5868bc11d1098
anlausch <anne-lauscher@web.de>
2018-10-30 08:08:06 +0100

    Adapt index sync script

--- scripts/sync_index.sh
+++ scripts/sync_index.sh
@@ -1,5 +1,5 @@
 SYSTEM=$1
-echo "Cleaning up db for env" $SYSTEM
+echo "Sync search index for env" $SYSTEM
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -16,6 +16,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 		DB="locdb-demo"
 		APP1="loc-db-demo"
 		APP2="loc-db-jobs-demo"
+    elif [ $SYSTEM = "tue" ]; then
+        DB="locdb-tue"
+        APP1="loc-db-tuebingen"
+        APP2="loc-db-jobs-tuebingen"
 	fi
 	INDEX="${DB}_br"
 	echo "Dropping index" $INDEX
