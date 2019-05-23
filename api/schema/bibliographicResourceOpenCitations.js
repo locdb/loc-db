62b789e08f6e593b288c111903f17ce5f09abc15
Kai Eckert <hallo@kaiec.de>
2019-05-23 15:12:20 +0200

    JS Schema accoring to OC example in paper

--- api/schema/bibliographicResourceOpenCitations.js
+++ api/schema/bibliographicResourceOpenCitations.js
@@ -7,20 +7,180 @@ const status = require('./enum.json').status;
 const ResourceEmbodiment = require('./resourceEmbodiment');
 
 var bibliographicResourceOpenCitations = new SchemaObject({
-    identifiers: [{type: Identifier}],
-    type: String,
+    iri: String,
+    a: [{type: String, enum: ["document", "article", "..."] }],
+    label: String,
+    identifier: [{
+        id: String,
+        iri: String,
+        a: {type: String, enum: ["unique_identifier"]},
+        type: String,
+        label: String
+    }],
     title: String,
     subtitle: String,
-    edition: String,
-    number: String, // e.g. number of an article in journal
-    contributors: [{type: AgentRole}],
-    publicationDate: Date,
-    status: {type: String, enum: [status.valid, status.external]},
-    cites: [{type: String}],
-    partOf: String, // link to other br
-    parts: [{type: BibliographicEntry}],
-    embodiedAs: [{type: ResourceEmbodiment}],
-    relatedDocument: Object
+    date: {
+        value: String,
+        a: {type: String, enum: ["year"]}
+    },
+    contributor: [{
+        iri: String,
+        a: {type: String, enum: ["role"]},
+        label: String,
+        role_type: {type: String, enum: ["author", "publisher", "editor"]},
+        role_of: {
+            iri: String,
+            a:  {type: String, enum: ["agent"]},
+            label: String,
+            gname: String,
+            fname: String,
+            identfier: [{
+                id: String,
+                iri: String,
+                a: {type: String, enum: ["unique_identifier"]},
+                type: String,
+                label: String
+            }],
+        },
+        next: String,
+    }],
+    format: [{
+        iri: String,
+        a: {type: String, enum: ["digital_format", "print"]},
+        label: String,
+        mime_type: String,
+        fpage: String,
+        lpage: String,
+    }],
+    reference: [{
+        iri: String,
+        a:  {type: String, enum: ["entry"]},
+        label: String,
+        content: String,
+        crossref: String,
+    }],
+    part_of: {
+        iri: String,
+        a: { type: String, enum: ["document", "periodical_issue", "periodical_volume", "periodical_journal", "proceedings", "..." ]},
+        label: String,
+        number: String,
+        identifier: [{
+            id: String,
+            iri: String,
+            a: {type: String, enum: ["unique_identifier"]},
+            type: String,
+            label: String
+        }],
+        title: String,
+        subtitle: String,
+        part_of: {
+            iri: String,
+            a: { type: String, enum: ["document", "periodical_issue", "periodical_volume", "periodical_journal", "proceedings", "..." ]},
+            label: String,
+            number: String,
+            identifier: [{
+                id: String,
+                iri: String,
+                a: {type: String, enum: ["unique_identifier"]},
+                type: String,
+                label: String
+            }],
+            title: String,
+            subtitle: String,
+            part_of: {
+                iri: String,
+                a: { type: String, enum: ["document", "periodical_issue", "periodical_volume", "periodical_journal", "proceedings", "..." ]},
+                label: String,
+                number: String,
+                identifier: [{
+                    id: String,
+                    iri: String,
+                    a: {type: String, enum: ["unique_identifier"]},
+                    type: String,
+                    label: String
+                }],
+                title: String,
+                subtitle: String,
+            }
+        }
+    },
+    citation: [{
+        iri: String,
+        a: {
+            type: String,
+            enum: ["document", "periodical_issue", "periodical_volume", "periodical_journal", "proceedings", "inproceedings", "..."]
+        },
+        label: String,
+        title: String,
+        date: {
+            value: String,
+            a: {type: String, enum: ["year"]}
+        },
+        format: [{
+            iri: String,
+            a: {type: String, enum: ["digital_format", "print"]},
+            label: String,
+            mime_type: String,
+            fpage: String,
+            lpage: String,
+        }],
+        part_of: {
+            iri: String,
+            a: {
+                type: String,
+                enum: ["document", "periodical_issue", "periodical_volume", "periodical_journal", "proceedings", "..."]
+            },
+            label: String,
+            number: String,
+            identifier: [{
+                id: String,
+                iri: String,
+                a: {type: String, enum: ["unique_identifier"]},
+                type: String,
+                label: String
+            }],
+            title: String,
+            subtitle: String,
+            part_of: {
+                iri: String,
+                a: {
+                    type: String,
+                    enum: ["document", "periodical_issue", "periodical_volume", "periodical_journal", "proceedings", "..."]
+                },
+                label: String,
+                number: String,
+                identifier: [{
+                    id: String,
+                    iri: String,
+                    a: {type: String, enum: ["unique_identifier"]},
+                    type: String,
+                    label: String
+                }],
+                title: String,
+                subtitle: String,
+                part_of: {
+                    iri: String,
+                    a: {
+                        type: String,
+                        enum: ["document", "periodical_issue", "periodical_volume", "periodical_journal", "proceedings", "..."]
+                    },
+                    label: String,
+                    number: String,
+                    identifier: [{
+                        id: String,
+                        iri: String,
+                        a: {type: String, enum: ["unique_identifier"]},
+                        type: String,
+                        label: String
+                    }],
+                    title: String,
+                    subtitle: String,
+                }
+            }
+        },
+    }],
 });
+
+
 
 module.exports = bibliographicResourceOpenCitations;
--- scripts/OCExporter/OCExporter.js
+++ scripts/OCExporter/OCExporter.js
@@ -12,10 +12,22 @@ OCExporter.prototype.result = {};
 
 
 OCExporter.prototype.parseFile = function(path, callback) {
-    console.log(pth.resolve());
     var brs = JSON.parse(fs.readFileSync(path));
     console.log(brs.length);
-    callback(brs);
+
+    var count = 0;
+    var result = []
+    for (var br of brs) {
+        var oc = {}
+        result.push(oc)
+        count++;
+        console.log(count + " " + JSON.stringify(oc));
+        if (count > 10) {
+            break;
+        }
+    }
+
+    callback(result);
 };
 
 
